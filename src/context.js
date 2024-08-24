efe39fb83f8a49e061b1bc52469f240431dc34d1
Muhammad Abdullah <sameeralvi156@gmail.com>
2024-08-24 14:38:58 +0500

    sets up basic functionality

--- src/context.js
+++ src/context.js
@@ -1,24 +1,48 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
 
 const table = {
   sports: 21,
   history: 23,
   politics: 24,
-}
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = ''
+const url = "";
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [waiting, setWaiting] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [correct, setCorrect] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [error, setError] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        isLoading,
+        waiting,
+        questions,
+        correct,
+        isModalOpen,
+        index,
+        error,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
